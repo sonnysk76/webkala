0e4a6aaf0c98b5857cf8529b0367d72aa2403430
shurtado976 <shurtado976@gmail.com>
2016-06-17 14:45:41 -0400

    paginas nuevas

---
+++ encabezado.php
@@ -0,0 +1,18 @@
+<div class="titulo card-3">
+<img class="logo" src="Lkala1.jpg" height="150px">
+<h1 class="tit">Cafeteri&#769;a Kala</h1>
+<div class="mnu card-2"><img src="mapa.png" height="100px" width="150px"><div class="page-featured-posts-grid-title-wrapper center-align">
+<h2 class="page-featured-posts-grid-title"><strong>UBICA</strong>CION</h2>
+</div>	
+</div>
+<div class="mnu card-2"><img src="nosotros.jpg" height="100px" width="150px"><div class="page-featured-posts-grid-title-wrapper center-align">
+<h2 class="page-featured-posts-grid-title">NOSO<strong>TROS</strong></h2>
+</div></div>
+<div class="mnu card-2"><img src="taza1.png" height="100px" width="100px"><div class="page-featured-posts-grid-title-wrapper center-align">
+<h2 class="page-featured-posts-grid-title">C<strong>ONTA</strong>CTO</h2>
+</div></div>
+</div>
+<div class="card c0"></div>
+<?php echo "hola"; ?>
+</body>
+</html>
---
+++ phpindice.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<HTML>
+<head> 
+<title>Cafeteria Kala</title>
+  <link rel="stylesheet" type="text/css" href="kala.css">
+
+</head>
+<body>
+<?php require 'encabezado.php'; ?>
